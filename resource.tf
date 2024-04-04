0f52de2a4cf4d75decce249c8f748b305693f741
unknown <gagankalleshwara13@gmail.com>
2024-04-04 13:40:09 +0530

    lklklk

--- resource.tf
+++ resource.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "first" {
   name     = "first_1"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "second" {
+  name     = "second_2"
+  location = "East US"
+}
